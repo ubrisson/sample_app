b1984bdbaf10f42bf7c65390145567ccd5499190
ubrisson <50323620+ubrisson@users.noreply.github.com>
2019-06-25 14:12:50 +0200

    Finish user edit, update, index, and destroy actions Fix location of tests

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
-    @other_user = users(:archer)
   end
 
   test 'unsuccessful edit' do
@@ -36,45 +35,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
 
-  test 'should redirect edit when not logged in' do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test 'should redirect update when not logged in' do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test 'should redirect edit when logged in as wrong user' do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test 'should redirect update when logged in as wrong user' do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test 'should not allow the admin attribute to be edited via the web' do
-    log_in_as(@other_user)
-    assert_not @other_user.admin?
-    patch user_path(@other_user), params: {
-      user: { password: 'foobar',
-              password_confirmation: 'foobar',
-              admin: true }
-    }
-    assert_not @other_user.reload.admin?
-  end
-
   test 'successful edit with friendly forwarding' do
     get edit_user_path(@user)
     log_in_as(@user)
@@ -104,8 +64,4 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert session[:forwarding_url].nil?
   end
 
-  test 'should redirect index when not logged in' do
-    get users_path
-    assert_redirected_to login_url
-  end
 end
