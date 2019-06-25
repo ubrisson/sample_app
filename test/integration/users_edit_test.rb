c41aa43660ce8f2b8fe0accd1907da91e025e898
ubrisson <50323620+ubrisson@users.noreply.github.com>
2019-06-25 13:12:59 +0200

    Fix url_form path bug & add admin column & test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,35 +1,34 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
   end
 
-  test "unsuccessful edit" do
+  test 'unsuccessful edit' do
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), params: { user: { name:  "",
-                                              email: "foo@invalid",
-                                              password:              "foo",
-                                              password_confirmation: "bar" } }
+    patch user_path(@user), params: { user: { name: '',
+                                              email: 'foo@invalid',
+                                              password: 'foo',
+                                              password_confirmation: 'bar' } }
 
     assert_template 'users/edit'
-    assert_select 'div.alert', "The form contains 4 errors."
+    assert_select 'div.alert', 'The form contains 4 errors.'
   end
 
-  test "successful edit" do
+  test 'successful edit' do
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
-    name  = "Foo Bar"
-    email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name:  name,
+    name  = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name: name,
                                               email: email,
-                                              password:              "",
-                                              password_confirmation: "" } }
+                                              password: '',
+                                              password_confirmation: '' } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
@@ -37,27 +36,27 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
 
-  test "should redirect edit when not logged in" do
+  test 'should redirect edit when not logged in' do
     get edit_user_path(@user)
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
-  test "should redirect update when not logged in" do
+  test 'should redirect update when not logged in' do
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
-  test "should redirect edit when logged in as wrong user" do
+  test 'should redirect edit when logged in as wrong user' do
     log_in_as(@other_user)
     get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
 
-  test "should redirect update when logged in as wrong user" do
+  test 'should redirect update when logged in as wrong user' do
     log_in_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
@@ -65,16 +64,27 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  test "successful edit with friendly forwarding" do
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password: 'foobar',
+              password_confirmation: 'foobar',
+              admin: true }
+    }
+    assert_not @other_user.reload.admin?
+  end
+
+  test 'successful edit with friendly forwarding' do
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
-    name  = "Foo Bar"
-    email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name:  name,
+    name  = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name: name,
                                               email: email,
-                                              password:              "",
-                                              password_confirmation: "" } }
+                                              password: '',
+                                              password_confirmation: '' } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
@@ -82,7 +92,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
 
-  test "friendly forwarding only once" do
+  test 'friendly forwarding only once' do
     get edit_user_path(@user)
     assert session[:forwarding_url] == edit_user_url(@user)
     assert_redirected_to login_url
@@ -94,7 +104,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert session[:forwarding_url].nil?
   end
 
-  test "should redirect index when not logged in" do
+  test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
   end
